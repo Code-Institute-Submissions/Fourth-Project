d577565065c9191f6d4c96bf62d0c782e556852b
Imalsha0330 <samaranayake.imalsha@gmail.com>
2023-07-26 15:13:10 +0200

    Updated Allowed host

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blogsite97.herokuapp.com', 'www.blogsite97.herokuapp.com']
+ALLOWED_HOSTS = ['blogsite97-38fbc9a66906.herokuapp.com']
 
 
 # Application definition
