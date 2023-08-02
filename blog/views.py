25ebcd9aaa87f1d008d65b663b106ba4754ccb34
Imalsha0330 <samaranayake.imalsha@gmail.com>
2023-08-02 11:56:57 +0200

    Updated post detail to details with "s"

--- blog/views.py
+++ blog/views.py
@@ -24,7 +24,7 @@ class PostDetail(View):
 
         return render(
             request,
-            "post_detail.html",
+            "post_details.html",
             {
                 "post": post,
                 "comments": comments,
@@ -55,7 +55,7 @@ class PostDetail(View):
 
         return render(
             request,
-            "post_detail.html",
+            "post_details.html",
             {
                 "post": post,
                 "comments": comments,
@@ -75,4 +75,4 @@ class PostLike(View):
         else:
             post.likes.add(request.user)
 
-        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+        return HttpResponseRedirect(reverse('post_details', args=[slug]))
